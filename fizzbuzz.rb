3eeb03f9521c755ef0c5c786b7fbfb267f7a5e83
Alice T <alicewtien@gmail.com>
2018-07-05 22:22:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-  
+def fizzbuzz(int)
+  if int % 3 == fixnum
+    puts "fizz"
+elsif int % 5 == fixnum
+    puts "fuzz"
+elsif int % 5 && int % 3
+  puts "fizzfuzz"
 end
