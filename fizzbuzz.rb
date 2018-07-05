ca19afb087f17bf90e26c63fc4dd3277982f30b6
Alice T <alicewtien@gmail.com>
2018-07-05 22:25:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     puts "fizz"
-elsif int % 5 == 0
+  elsif int % 5 == 0
     puts "buzz"
-elsif int % 5 && int % 3 == 0
+  elsif int % 5 && int % 3 == 0
   puts "fizzbuzz"
 else
   puts "nil"
